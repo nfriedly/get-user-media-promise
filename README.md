571656e16e8bcafc61e54456688271be4bfc94ab
Nathan Friedly <nathan@nfriedly.com>
2016-03-08 12:23:48 -0500

    initial build appears to be working

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # get-user-media-promise
+
+Light-weight wrapper for the newer [Promise]-based [MediaDevices.getUserMedia] API with a fallback to the deprecated [navigator.getUserMedia] (and it's prefixed variants)
+
+Note: this will only work in web browsers.
+
+Usage: 
+
+ * With a bundler such as Require.js, Browserify, or WebPack: require() the module and it returns a `getUserMedia()` method that will always return a Promise or Promise-like object.
+ * As a stand-alone pollyfill: when neither `define()` nor `module` are defined, the module will go into pollyfill mode, setting `navigator.mediaDevices.getUserMedia()` if that method is not already defined and doing nothing if it is.
+
+
+[MediaDevices.getUserMedia]: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+[Promise]: https://developer.mozilla.org/en-US/docs/Web/API/Promise
+[navigator.getUserMedia]: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getUserMedia
---
+++ test/test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var assert = require('assert');
+var getUserMedia = require('../lib/get-user-media-promise');
+
+//var chrome = navigator.userAgent.indexOf('Chrome') >= 0;
+var firefox = navigator.userAgent.indexOf('Firefox') >= 0;
+var travis = process.env.TRAVIS;
+
+describe('getUserMedia', function() {
+  it('should exist', function() {
+    assert(getUserMedia)
+  });
+
+  if (firefox && travis) {
+    // firefox can auto-approve gum requests but cannot fake the output. So, on travis CI it always rejects them with NO_DEVICES_FOUND
+    it('should reject when there are no audio devices available', function(done) {
+      getUserMedia({audio: true, video: true})
+        .then(function() {
+          done(new Error('.then() should not be called when there are no media devices available'))
+        })
+        .catch(function() {
+          done();
+        });
+    })
+  } else {
+    it('should return a MediaStream instance in supported browsers', function() {
+      return getUserMedia({audio: true})
+    });
+  }
+
+  // todo: add additional tests around what happens when various required globals don't exist
+  // todo: test with require.js and in pollyfill mode
+});
